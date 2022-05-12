1136e5561f0fefabc279e237e71c86bf06ea57f0
changkeun <ckdrmsdk@gmail.com>
2022-05-12 18:00:19 +0900

    fix: 폴더 수정

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,16 @@
 import styles from './Routes.module.scss'
 import { Routes, Route } from 'react-router-dom'
-import TodoList from './TodoList'
 import Weather from './Weathers'
-import GNB from 'routes/_shared/GNB'
 
 const App = () => {
   return (
     <div className={styles.appWrapper}>
-      <GNB />
       <div className={styles.app}>
         <Routes>
-          <Route path='/' element={<TodoList />} />
-          <Route path='todo' element={<TodoList />} />
           <Route path='weather' element={<Weather />}>
             <Route path=':city' element={<Weather />} />
           </Route>
+          <Route path='*' element={<Weather />} />
         </Routes>
       </div>
     </div>
